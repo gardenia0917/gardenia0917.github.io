8b6f5d1c728821a57124d8470ee8eb1017dd1221
gardenia1109 <gardenia1109@naver.com>
2022-06-24 09:23:48 +0900

    토글

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[CodeStates] 변수",
+        "excerpt":"test  ","categories": ["codestatessec1"],
+        "tags": ["CodeStates","js"],
+        "url": "/codestatessec1/%EB%B3%80%EC%88%98/",
+        "teaser": null
+      }]
